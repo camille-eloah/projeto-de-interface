69980a1c1361c95f3b73854b23062e410489c1a1
camille-eloah <camelogithub@gmail.com>
2025-05-08 16:48:35 -0300

    first commit

---
+++ src/FirstComponent.jsx
@@ -0,0 +1,17 @@
+function FirstComponent () {
+    
+    const nome = "Camille Eloah"
+
+    return (
+        <>
+        <div>
+            <h1>Hello, World!</h1>
+        </div>
+        <div>
+            <p>Seu nome é {nome}</p>
+        </div>
+        </>
+    )
+}
+
+export default FirstComponent
