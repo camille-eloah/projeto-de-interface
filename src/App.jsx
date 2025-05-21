932c7bc5814c292fb305764b1832dbd449df67e6
camille-eloah <camelogithub@gmail.com>
2025-05-21 13:37:38 -0300

    21-05-2025

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Escopo from './components/Escopo'
 import EscopoReduz from './components/EscopoReduz'
 import Info1v from './components/Info1V'
 
+import Props from './components/Props'
+import Card from './components/Card'
+import Children from './components/Children'
 
 function App() {
 
@@ -14,8 +17,13 @@ function App() {
       <EscopoReduz/>
       */}
 
-      <Info1v/>
-      <Conditional/>
+      <Props nome={"React PIU"}/>
+      <Card/> 
+      <br/> 
+      <Children> 
+        <Card/> 
+      </Children>
+
     </>
   )
 }
